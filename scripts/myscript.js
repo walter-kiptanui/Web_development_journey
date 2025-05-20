d5603a70eaa9937d98929a5ede0c57630076d3f4
walter-kiptanui <walterkiptanui5@gmail.com>
2025-05-20 15:58:32 +0300

    Adding an image changer to a webpage

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,3 +1,11 @@
-var myHeading = document.querySelector("h1")
+/* global variable */
+var outlookImage = document.querySelector('img');
 
-myHeading.textContent = "Be kind";
+outlookImage.onclick = function() {
+    var myImages = outlookImage.getAttribute('src');
+    if (myImages === 'images/aleksandra-dementeva-SRgl8pvoFC0-unsplash.jpg') {
+        outlookImage.setAttribute('src', 'images/royce-fonseca-sgm8_Tn7t5E-unsplash.jpg');
+    }else {
+        outlookImage.setAttribute('src', 'images/aleksandra-dementeva-SRgl8pvoFC0-unsplash.jpg');
+    }
+}
