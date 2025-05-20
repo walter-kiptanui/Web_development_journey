59c44739e97c888282b3d4a3893bfaf0021e51de
walter-kiptanui <walterkiptanui5@gmail.com>
2025-05-20 18:32:57 +0300

    Adding a personalized message to a webpage

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -9,3 +9,26 @@ outlookImage.onclick = function() {
         outlookImage.setAttribute('src', 'images/aleksandra-dementeva-SRgl8pvoFC0-unsplash.jpg');
     }
 }
+
+//Personalized welcome message code
+var nameButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+    'use strict';
+    var myName = window.prompt('Please enter your name:');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Have a nice day,' + myName;
+}
+
+if (!localStorage.getItem('name')) {
+    setUserName();
+}else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Have a nice day,' + storedName;
+}
+
+nameButton.onclick = function() {
+    'use strict';
+    setUserName();
+};
